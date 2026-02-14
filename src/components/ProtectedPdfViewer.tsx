bbfe346728a039ea6970c8fd9686a6879a65bc91
Anwav shah <anwavshah@outlook.com>
2026-02-14 17:34:37 +0545

    Website update

--- src/components/ProtectedPdfViewer.tsx
+++ src/components/ProtectedPdfViewer.tsx
@@ -6,7 +6,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
 // Set PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdfjs/pdf.worker.min.js';
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdfjs/pdf.worker.mjs';
 
 interface Article {
   id: number;
